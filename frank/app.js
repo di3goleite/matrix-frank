0db77bdcc74b7e1130bb999ee51fbf4ea6857a29
Diego Leite <diegojleite@gmail.com>
2016-12-01 10:44:16 -0300

    Sending requests to the server

--- frank/app.js
+++ frank/app.js
@@ -1,6 +1,11 @@
+var request = require('request');
+var url = 'http://192.168.15.245:8080/webhooks';
+
 matrix.led('blue').render();
 
 matrix.init('demographics').then(function(data) {
     matrix.led('red').render();
     console.log('>>>>>>>>>>>>>>>>', data);
+
+    request({url: url, method: "POST", json: data});
 });
